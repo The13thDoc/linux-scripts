a1a477235182202a9d4c5177e3467f55d79977fa
Jon <thethneedinglorax@gmail.com>
2015-03-27 23:24:55 -0500

    Update flac-check.sh
    Some changes that occurred but weren't uploaded to Git. 
     - Iterates each file on the Terminal
     - Leaves a log file in the working directory of any files with errors

--- flac-check.sh
+++ flac-check.sh
@@ -7,9 +7,20 @@ shopt -s globstar
 # Exact link is unknown. It has been modified to suit my needs.
 ##==
 
+filename="flac-test.txt"
+
+echo 'Starting check...' > $filename
+#echo 'Only display files with errors' >> $filename
+echo '' >> $filename
+
 # /media/Elements/Music/Metallica
 # ./**/*.flac
 for file in ./**/*.flac; do
 	echo $file
-    flac -wst "$file" 2>/dev/null || printf '%3d %s\n' "$?" "$file" >> status.txt
+    #echo 'Checking: ' $file >> $filename
+    flac -wst "$file" 2>/dev/null || printf '%3d %s\n' "$?" "$file" >> $filename
+
 done
+
+echo 'Done.' >> $filename
+echo '' >> $filename
